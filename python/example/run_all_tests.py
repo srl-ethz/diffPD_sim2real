78e88e37aaa290bca49cf1f1c98fccea3415273c
Tao Du <taodu@csail.mit.edu>
2020-06-28 12:10:09 -0400

    Simplify run_all_tests.

--- python/example/run_all_tests.py
+++ python/example/run_all_tests.py
@@ -1,20 +1,26 @@
 import sys
 sys.path.append('../')
 
+from importlib import import_module
 from py_diff_pd.common.common import print_ok, print_error
 
-# Display.
-from render_hex_mesh import test_render_hex_mesh
-from render_quad_mesh import test_render_quad_mesh
-
 if __name__ == '__main__':
+    # If you want to add a new test, simply add its name here --- you can find their names from README.md.
     tests = [
-        (test_render_hex_mesh, 'render_hex_mesh'),
-        (test_render_quad_mesh, 'render_quad_mesh'),
+        'render_hex_mesh',
+        'render_quad_mesh',
     ]
-    for test_func, test_name in tests:
+
+    failure_cnt = 0
+    for name in tests:
+        test_func_name = 'test_{}'.format(name)
+        module_name = name
+        test_func = getattr(import_module(module_name), test_func_name)
         if test_func(verbose=False):
-            print_ok('[{}] PASSED.'.format(test_name))
+            print_ok('[{}] PASSED.'.format(name))
         else:
-            print_error('[{}] FAILED.'.format(test_name))
-            sys.exit(-1)
+            print_error('[{}] FAILED.'.format(name))
+            failure_cnt += 1
+    print('{}/{} tests failed.'.format(failure_cnt, len(tests)))
+    if failure_cnt > 0:
+        sys.exit(-1)
