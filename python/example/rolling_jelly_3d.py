6539fbdd366e1df8c8dcb601d0093e1ef04fe819
Tao Du <taodu@csail.mit.edu>
2020-07-24 23:46:40 -0400

    Added rolling_jelly_3d. [skip ci]

--- python/example/rolling_jelly_3d.py
+++ python/example/rolling_jelly_3d.py
@@ -11,15 +11,15 @@ from py_diff_pd.common.common import ndarray, create_folder
 from py_diff_pd.common.common import print_info, print_ok, print_error
 from py_diff_pd.common.grad_check import check_gradients
 from py_diff_pd.core.py_diff_pd_core import StdRealVector
-from py_diff_pd.env.cantilever_env_3d import CantileverEnv3d
+from py_diff_pd.env.rolling_jelly_env_3d import RollingJellyEnv3d
 
 if __name__ == '__main__':
     seed = 42
-    folder = Path('cantilever_3d')
-    refinement = 4
+    folder = Path('rolling_jelly_env_3d')
+    refinement = 8
     youngs_modulus = 1e6
-    poissons_ratio = 0.45
-    env = CantileverEnv3d(seed, folder, { 'refinement': refinement,
+    poissons_ratio = 0.49
+    env = RollingJellyEnv3d(seed, folder, { 'refinement': refinement,
         'youngs_modulus': youngs_modulus,
         'poissons_ratio': poissons_ratio })
     deformable = env.deformable()
@@ -34,22 +34,19 @@ if __name__ == '__main__':
             'method': 1, 'bfgs_history_size': 10 }
     )
 
+    dt = 4e-3
+    frame_num = 50
+
     # Compute the initial state.
     dofs = deformable.dofs()
     act_dofs = deformable.act_dofs()
+    q_gt = ndarray([0.0, 0.0, 0.15])
+    v_gt = ndarray([2, 0.5, -8])
     q0 = env.default_init_position()
+    q0 = (q0.reshape((-1, 3)) + q_gt).ravel()
     v0 = np.zeros(dofs)
-    dt = 1e-2
-    frame_num = 100
+    v0 = (v0.reshape((-1, 3)) + v_gt).ravel()
     a0 = [np.zeros(act_dofs) for _ in range(frame_num)]
-    vertex_num = int(dofs // 3)
-    f0 = np.zeros((vertex_num, 3))
-    f0[:, 2] = 1.0
-    f0 = f0.ravel()
-    f0 = [f0 for _ in range(frame_num)]
-    _, info = env.simulate(dt, frame_num, methods[0], opts[0], q0, v0, a0, f0, require_grad=False, vis_folder=None)
-    q0 = info['q'][-1]
-    v0 = np.zeros(dofs)
     f0 = [np.zeros(dofs) for _ in range(frame_num)]
 
     # Generate groudtruth motion.
@@ -57,8 +54,8 @@ if __name__ == '__main__':
 
     # Optimization.
     # Decision variables: log(E), log(nu).
-    x_lb = ndarray([np.log(5e5), np.log(0.35)])
-    x_ub = ndarray([np.log(5e6), np.log(0.49)])
+    x_lb = ndarray([np.log(5e5), np.log(0.45)])
+    x_ub = ndarray([np.log(5e6), np.log(0.495)])
     x_init = np.random.uniform(low=x_lb, high=x_ub)
     bounds = scipy.optimize.Bounds(x_lb, x_ub)
     data = {}
@@ -67,7 +64,7 @@ if __name__ == '__main__':
         def loss_and_grad(x):
             E = np.exp(x[0])
             nu = np.exp(x[1])
-            env_opt = CantileverEnv3d(seed, folder, { 'refinement': refinement, 'youngs_modulus': E,
+            env_opt = RollingJellyEnv3d(seed, folder, { 'refinement': refinement, 'youngs_modulus': E,
                 'poissons_ratio': nu })
             loss, _, info = env_opt.simulate(dt, frame_num, method, opt, q0, v0, a0, f0, require_grad=True, vis_folder=None)
             grad = info['material_parameter_gradients']
@@ -95,6 +92,6 @@ if __name__ == '__main__':
         # Visualize results.
         E = np.exp(x_final[0])
         nu = np.exp(x_final[1])
-        env_opt = CantileverEnv3d(seed, folder, { 'refinement': refinement, 'youngs_modulus': E,
+        env_opt = RollingJellyEnv3d(seed, folder, { 'refinement': refinement, 'youngs_modulus': E,
             'poissons_ratio': nu })
         env_opt.simulate(dt, frame_num, method, opt, q0, v0, a0, f0, require_grad=False, vis_folder=method)
