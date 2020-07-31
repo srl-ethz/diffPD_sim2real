8fe6c8faabc15c4f79b027d301acea82bcbf66f1
Tao Du <taodu@csail.mit.edu>
2020-07-30 22:17:05 -0400

    Resolved mysterious Pardiso bug kudos to fish.

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -4,31 +4,21 @@
 #include "common/common.h"
 #include "solver/pardiso_solver.h"
 
-int main() {
+int main(int argc, char* argv[]) {
 #ifdef PARDISO_AVAILABLE
-    const int n = 8;
-    SparseMatrixElements nonzeros;
-    const std::vector<int> nonzeros_rows = {
-        0, 0, 0, 0, 1, 1, 1, 2, 2, 3, 3, 4, 4, 4, 5, 5, 6, 7,
-    };
-    const std::vector<int> nonzeros_cols = {
-        0, 2, 5, 6, 1, 2, 4, 2, 7, 3, 6, 4, 5, 6, 5, 7, 6, 7
-    };
-    const std::vector<real> nonzeros_vals = {
-        7, 1, 2, 7, -4, 8, 2, 1, 5, 7, 9, 5, 1, 5, 0, 5, 11, 5
-    };
-    const int nonzero_num = static_cast<int>(nonzeros_rows.size());
-    for (int i = 0; i < nonzero_num; ++i) {
-        const int r = nonzeros_rows[i];
-        const int c = nonzeros_cols[i];
-        const real val = nonzeros_vals[i];
-        nonzeros.push_back(Eigen::Triplet<real>(r, c, val));
-        if (r < c) nonzeros.push_back(Eigen::Triplet<real>(c, r, val));
-    }
+    int n = 32;
+    if (argc > 1) n = std::stoi(std::string(argv[1]));
+    std::cout << "Solving a randomly generated " << n << " x " << n << " matrix" << std::endl;
+    MatrixXr A_dense = MatrixXr::Random(n, n);
+    SparseMatrix A_sparse = A_dense.sparseView(1, 0.9);
+    SparseMatrix At = A_sparse.transpose();
+    SparseMatrix AtA = At * A_sparse;
+    SparseMatrixElements nonzeros = FromSparseMatrix(AtA);
     // Ensure this is SPD.
-    for (int i = 0; i < n; ++i) nonzeros.push_back(Eigen::Triplet<real>(i, i, 100));
+    for (int i = 0; i < n; ++i) nonzeros.push_back(Eigen::Triplet<real>(i, i, 1));
     const VectorXr b = VectorXr::Random(n);
     const SparseMatrix A = ToSparseMatrix(n, n, nonzeros);
+    std::cout << A.nonZeros() * 100.0 / (n * n) << "% elements are nonzero." << std::endl;
     const int thread_cnt = 4;
     std::map<std::string, real> options;
     options["thread_ct"] = thread_cnt;
