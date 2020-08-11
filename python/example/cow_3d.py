e8273dc7e962a48d3027c3c798e51c6f0ceb360d
Seb <sebastien.wah@me.com>
2020-08-11 16:41:49 -0700

    Added Spot quadruped

---
+++ python/example/cow_3d.py
@@ -0,0 +1,130 @@
+import sys
+sys.path.append('../')
+
+from pathlib import Path
+import time
+import numpy as np
+import scipy.optimize
+import pickle
+
+from py_diff_pd.common.common import ndarray, create_folder, rpy_to_rotation, rpy_to_rotation_gradient
+from py_diff_pd.common.common import print_info, print_ok, print_error
+from py_diff_pd.common.grad_check import check_gradients
+from py_diff_pd.core.py_diff_pd_core import StdRealVector
+from py_diff_pd.env.cow_env_3d import CowEnv3d
+
+if __name__ == '__main__':
+    seed = 40
+    folder = Path('cow_3d')
+    refinement = 8
+    act_max = 2.0
+    youngs_modulus = 1e6
+    poissons_ratio = 0.49
+    target_com = ndarray([0.5, 0.5, 0.2])
+    env = CowEnv3d(seed, folder, { 'refinement': refinement,
+        'youngs_modulus': youngs_modulus,
+        'poissons_ratio': poissons_ratio })
+    deformable = env.deformable()
+    leg_indices = env._leg_indices
+
+    # Optimization parameters.
+    thread_ct = 4
+    newton_opt = { 'max_newton_iter': 500, 'max_ls_iter': 20, 'abs_tol': 1e-9, 'rel_tol': 1e-4, 'verbose': 0, 'thread_ct': thread_ct }
+    pd_opt = { 'max_pd_iter': 500, 'max_ls_iter': 1, 'abs_tol': 1e-9, 'rel_tol': 1e-4, 'verbose': 0, 'thread_ct': thread_ct,
+        'use_bfgs': 1, 'bfgs_history_size': 10 }
+    methods = ('newton_pcg', 'newton_cholesky', 'pd_eigen')
+    opts = (newton_opt, newton_opt, pd_opt)
+
+    dt = 5e-3
+    frame_num = 75
+
+    # Initial state.
+    dofs = deformable.dofs()
+    act_dofs = deformable.act_dofs()
+    q0 = env.default_init_position()
+    v0 = np.zeros(dofs)
+    a0 = [2*np.ones(act_dofs) for _ in range(frame_num)]
+    f0 = [np.zeros(dofs) for _ in range(frame_num)]
+
+
+    # Optimization.
+    # Variables to be optimized:
+    x_lb = ndarray([0, 0, 2*np.pi / frame_num])#, 0])
+    x_ub = ndarray([1, 1, 2*np.pi / 14])#, np.pi/2])
+    #x_init = ndarray([1, 1, 2*np.pi/14])
+    x_init = ndarray([0.5* np.random.random() + 0.5, 0.5* np.random.random() + 0.5, np.random.uniform(x_lb[2], x_ub[2])])
+    # Visualize initial guess.
+    def variable_to_states(x, return_jac):
+        A_f = x[0]
+        A_b = x[1]
+        w = x[2]
+        jac = [np.ones((3, act_dofs)) for _ in range(frame_num)]
+        a = [np.zeros(act_dofs) for _ in range(frame_num)]
+
+        for i in range(frame_num):
+            for key, indcs in leg_indices.items():
+                if key[-1] == 'F':
+                    for idx in indcs:
+                        if key[0] == 'F':
+                            a[i][idx] = act_max * (1 + A_f*np.sin(w*i)) / 2
+                            jac[i][:, idx] = [np.sin(w*i), 0, A_f*i*np.cos(w*i)]
+                        else:
+                            a[i][idx] = act_max * (1 + A_b*np.sin(w*i)) / 2
+                            jac[i][:, idx] = [0, np.sin(w*i), A_b*i*np.cos(w*i)]
+                else:
+                    for idx in indcs:
+                        if key[0] =='F':
+                            a[i][idx] =  act_max * (1 - A_f*np.sin(w*i)) / 2
+                            jac[i][:, idx] = [-np.sin(w*i), 0, -A_f*i*np.cos(w*i)]
+                        else:
+                            a[i][idx] = act_max * (1 - A_b*np.sin(w*i)) / 2
+                            jac[i][:, idx] = [0, -np.sin(w*i), -A_b*i*np.cos(w*i)]
+        jac = [act_max * col / 2 for col in jac]
+        if return_jac:
+            return a, jac
+        return a
+
+
+    a_init = variable_to_states(x_init, False)
+    env.simulate(dt, frame_num, methods[2], opts[2], q0, v0, a_init, f0, require_grad=False, vis_folder='init')
+
+    bounds = scipy.optimize.Bounds(x_lb, x_ub)
+    data = {}
+    # for method, opt in zip(methods, opts):
+    data[methods[2]] = []
+    def loss_and_grad(x):
+        a, jac = variable_to_states(x, True)
+        loss, _, info = env.simulate(dt, frame_num, methods[2], opts[2], q0, v0, a, f0, require_grad=True, vis_folder=None)
+        # Assemble the gradients.
+        act_grad = info['actuator_gradients']
+        grad = ndarray([jac[i].dot(np.transpose(act_grad[i])) for i in range(frame_num)])
+        grad = np.sum(grad, axis=0)
+
+        print('loss: {:8.3f}, |grad|: {:8.3f}, A_f: {:8.5f}, A_b: {:8.5f}, w: {:8.5f}, forward time: {:6.3f}s, backward time: {:6.3f}s'.format(
+            loss, np.linalg.norm(grad), x[0], x[1], x[2], info['forward_time'], info['backward_time']))
+        single_data = {}
+        single_data['loss'] = loss
+        single_data['grad'] = np.copy(grad)
+        single_data['x'] = np.copy(x)
+        single_data['forward_time'] = info['forward_time']
+        single_data['backward_time'] = info['backward_time']
+        data[methods[2]].append(single_data)
+        return loss, np.copy(grad)
+
+    # Use the two lines below to sanity check the gradients.
+    # Note that you might need to fine tune the rel_tol in opt to make it work.
+    # from py_diff_pd.common.grad_check import check_gradients
+    # check_gradients(loss_and_grad, x_init, eps=1e-6)
+
+    t0 = time.time()
+    result = scipy.optimize.minimize(loss_and_grad, np.copy(x_init),
+        method='L-BFGS-B', jac=True, bounds=bounds, options={ 'ftol': 1e-3 })
+    t1 = time.time()
+    print(result.success)
+    x_final = result.x
+    print_info('Optimizing with {} finished in {:6.3f} seconds'.format(methods[2], t1 - t0))
+    pickle.dump(data, open(folder / 'data_{:04d}_threads.bin'.format(thread_ct), 'wb'))
+
+    # Visualize results.
+    a_final = variable_to_states(x_final, False)
+    env.simulate(dt, frame_num, methods[2], opts[2], q0, v0, a_final, f0, require_grad=False, vis_folder=methods[2])
---
+++ python/py_diff_pd/env/cow_env_3d.py
@@ -0,0 +1,161 @@
+import time
+from pathlib import Path
+import os
+
+import numpy as np
+
+from py_diff_pd.env.env_base import EnvBase
+from py_diff_pd.common.common import create_folder, ndarray
+from py_diff_pd.common.mesh import generate_hex_mesh, get_contact_vertex
+from py_diff_pd.common.display import render_hex_mesh, export_gif
+from py_diff_pd.core.py_diff_pd_core import Mesh3d, Deformable3d, StdRealVector
+from py_diff_pd.common.project_path import root_path
+
+class CowEnv3d(EnvBase):
+    # Refinement is an integer controlling the resolution of the mesh.
+    def __init__(self, seed, folder, options):
+        EnvBase.__init__(self, folder)
+
+        np.random.seed(seed)
+        create_folder(folder, exist_ok=True)
+
+        youngs_modulus = options['youngs_modulus']
+        poissons_ratio = options['poissons_ratio']
+
+        # Mesh parameters.
+        la = youngs_modulus * poissons_ratio / ((1 + poissons_ratio) * (1 - 2 * poissons_ratio))
+        mu = youngs_modulus / (2 * (1 + poissons_ratio))
+        density = 5e2
+
+        bin_file_name = Path(root_path) / 'asset' / 'mesh' / 'spot.bin'
+        mesh = Mesh3d()
+        mesh.Initialize(str(bin_file_name))
+        cow_size = 0.1
+        # Rescale the mesh.
+        mesh.Scale(cow_size)
+        tmp_bin_file_name = '.tmp.bin'
+        mesh.SaveToFile(tmp_bin_file_name)
+
+        dx =  1/15 * cow_size
+
+        deformable = Deformable3d()
+        deformable.Initialize(tmp_bin_file_name, density, 'none', youngs_modulus, poissons_ratio)
+        os.remove(tmp_bin_file_name)
+        # Elasticity.
+        deformable.AddPdEnergy('corotated', [2 * mu,], [])
+        deformable.AddPdEnergy('volume', [la,], [])
+        # State-based forces.
+        deformable.AddStateForce('gravity', [0, 0, -9.81])
+        # Collisions.
+        vertex_num = mesh.NumOfVertices()
+        friction_node_idx = []
+        for i in range(vertex_num):
+            vx, vy, vz = mesh.py_vertex(i)
+            if vz < dx / 2:
+                friction_node_idx.append(i)
+        # Uncomment the code below if you would like to display the contact set for a sanity check:
+
+        # import matplotlib.pyplot as plt
+        # from mpl_toolkits.mplot3d import Axes3D
+        # fig = plt.figure()
+        # ax = fig.add_subplot(111, projection='3d')
+        # v = ndarray([ndarray(mesh.py_vertex(idx)) for idx in friction_node_idx])
+        # ax.scatter(v[:, 0], v[:, 1], v[:, 2])
+        # plt.show()
+
+        # Friction_node_idx = all vertices on the edge.
+        deformable.SetFrictionalBoundary('planar', [0.0, 0.0, 1.0, 0.0], friction_node_idx)
+
+        # Actuation: we have 4 legs and each leg has 4 muscles.
+        # Convention: F (Front): positive x; R (Rear): negative x;
+        #             L (Left): positive y; R (Right): negative y.
+        leg_indices = {}
+        act_indices = []
+        count = 0
+        leg_z_length = 4
+        body_x_length = 9
+        body_y_length = 6
+        foot_x = 2
+        foot_y = 2
+        x_offset = 1
+        y_offset = 1
+        element_num = mesh.NumOfElements()
+        for i in range(element_num):
+            v_idx = ndarray(mesh.py_element(i))
+            # Obtain the center of this cell.
+            com = 0
+            for vi in v_idx:
+                com += ndarray(mesh.py_vertex(int(vi)))
+            com /= len(v_idx)
+            x_idx, y_idx, z_idx = com / dx
+
+            if z_idx >= leg_z_length: continue
+            if x_idx >= body_x_length + x_offset or \
+                (x_idx >= foot_x + x_offset and x_idx <= body_x_length  + x_offset - foot_x): continue
+            if y_idx >= foot_y + y_offset and y_idx <= body_y_length + y_offset - foot_y: continue
+            # First, determine which leg the voxel is in.
+            #print("i: {}, x: {}, y: {}, z: {}".format(i, x_idx, y_idx, z_idx))
+            leg_key = ('F' if x_idx >= body_x_length * 0.5 + x_offset else 'R') \
+                + ('L' if y_idx >= body_y_length * 0.5 + y_offset else 'R')
+            # Second, determine which muscle this voxel should be in front (F) or back (B).
+            if leg_key[0] == 'F':
+                x_idx -= body_x_length - foot_x
+            muscle_key = ('F' if x_idx >= foot_x/2 + x_offset else 'B') \
+
+            key = leg_key + muscle_key
+            if key not in leg_indices:
+                leg_indices[key] = [count,]
+                count += 1
+                act_indices.append(i)
+            else:
+                leg_indices[key].append(count)
+                act_indices.append(i)
+                count += 1
+        deformable.AddActuation(5e5, [0.0, 0.0, 1.0], act_indices)
+
+        # Initial states.
+        dofs = deformable.dofs()
+        act_dofs = deformable.act_dofs()
+        q0 = ndarray(mesh.py_vertices())
+        v0 = np.zeros(dofs)
+        f_ext = np.zeros(dofs)
+
+        # Data members.
+        self._deformable = deformable
+        self._q0 = q0
+        self._v0 = v0
+        self._f_ext = f_ext
+        self._youngs_modulus = youngs_modulus
+        self._poissons_ratio = poissons_ratio
+        self._stepwise_loss = False
+        self._leg_indices = leg_indices
+
+    def material_stiffness_differential(self, youngs_modulus, poissons_ratio):
+        jac = self._material_jacobian(youngs_modulus, poissons_ratio)
+        jac_total = np.zeros((2, 2))
+        jac_total[0] = 2 * jac[1]
+        jac_total[1] = jac[0]
+        return jac_total
+
+    def is_dirichlet_dof(self, dof):
+        return False
+
+    def _display_mesh(self, mesh_file, file_name):
+        mesh = Mesh3d()
+        mesh.Initialize(mesh_file)
+        render_hex_mesh(mesh, file_name=file_name,
+            resolution=(400, 400), sample=8, transforms=[
+                ('s', 4)
+            ])
+
+    def _loss_and_grad(self, q, v):
+        # Compute the center of mass.
+        com = np.mean(q.reshape((-1, 3)), axis=0)
+        # Compute loss.
+        loss = -com[0]
+        # Compute grad.
+        grad_q = np.zeros(q.size)
+        vertex_num = int(q.size // 3)
+        grad_q[::3] = -1 / vertex_num
+        grad_v = np.zeros(v.size)
+        return loss, grad_q, grad_v
