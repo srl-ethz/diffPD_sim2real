d393a3121e8d49dd980d1ca7cf648e68c0bf1940
Tao Du <taodu@csail.mit.edu>
2020-09-09 00:09:06 -0400

    Normalize tendon routing loss. [skip ci]

--- python/example/print_tendon_routing_3d.py
+++ python/example/print_tendon_routing_3d.py
@@ -12,6 +12,15 @@ from py_diff_pd.common.common import ndarray
 
 if __name__ == '__main__':
     folder = Path('tendon_routing_3d')
+    try:
+        data = pickle.load(open(folder / 'data_0008_threads.bin', 'rb'))
+    except:
+        print_error('Log file not found.')
+    loss_l, loss_h = data['loss_range']
+    print_info('Loss range: {:3f}, {:3f}'.format(loss_l, loss_h))
+    def normalize_loss(unnormalized_loss):
+        return (unnormalized_loss - loss_l) / (loss_h - loss_l)
+
     for thread_ct in [8]:
         data_file = Path('tendon_routing_3d') / 'data_{:04d}_threads.bin'.format(thread_ct)
         if data_file.exists():
@@ -22,6 +31,7 @@ if __name__ == '__main__':
                 avg_forward = 0
                 average_backward = 0
                 for d in data[method]:
+                    d['loss'] = normalize_loss(d['loss'])
                     print('loss: {:8.3f}, |grad|: {:8.3f}, forward time: {:6.3f}s, backward time: {:6.3f}s'.format(
                         d['loss'], np.linalg.norm(d['grad']), d['forward_time'], d['backward_time']))
                     total_time += d['forward_time'] + d['backward_time']
--- python/example/tendon_routing_3d.py
+++ python/example/tendon_routing_3d.py
@@ -72,7 +72,22 @@ if __name__ == '__main__':
     env.simulate(dt, frame_num, methods[0], opts[0], q0, v0, [a_init for _ in range(frame_num)], f0, require_grad=False, vis_folder='init')
 
     bounds = scipy.optimize.Bounds(x_lb, x_ub)
-    data = {}
+
+    # Normalize the loss.
+    rand_state = np.random.get_state()
+    random_guess_num = 16
+    random_loss = []
+    for _ in range(random_guess_num):
+        x_rand = np.random.uniform(low=x_lb, high=x_ub)
+        a = variable_to_act(x_rand)
+        loss, _ = env.simulate(dt, frame_num, methods[2], opts[2], q0, v0, [a for _ in range(frame_num)], f0,
+            require_grad=False, vis_folder=None)
+        random_loss.append(loss)
+    loss_range = ndarray([0, np.mean(random_loss)])
+    print_info('Loss range: {:3f}, {:3f}'.format(loss_range[0], loss_range[1]))
+    np.random.set_state(rand_state)
+
+    data = { 'loss_range': loss_range }
     for method, opt in zip(methods, opts):
         data[method] = []
         def loss_and_grad(x):
