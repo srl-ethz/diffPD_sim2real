6b7d2d35ea8b30deed4de2b0d53760cd2f02c327
Tao Du <taodu@csail.mit.edu>
2020-09-13 22:28:08 -0400

    Change 1e-4 to 1e-6 in render_rolling_jelly_3d. [skip ci]

--- python/example/render_rolling_jelly_3d.py
+++ python/example/render_rolling_jelly_3d.py
@@ -22,7 +22,7 @@ if __name__ == '__main__':
     deformable = env.deformable()
 
     method = 'pd_eigen'
-    opt = { 'max_pd_iter': 5000, 'max_ls_iter': 10, 'abs_tol': 1e-9, 'rel_tol': 1e-4, 'verbose': 0, 'thread_ct': 4,
+    opt = { 'max_pd_iter': 5000, 'max_ls_iter': 10, 'abs_tol': 1e-9, 'rel_tol': 1e-6, 'verbose': 0, 'thread_ct': 4,
             'use_bfgs': 1, 'bfgs_history_size': 10 }
 
     dt = 5e-3
