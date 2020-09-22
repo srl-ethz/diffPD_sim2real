94a98f68369e8ea225aed3a0cf504fac06d1a840
Tao Du <taodu@csail.mit.edu>
2020-09-21 22:29:58 -0400

    Added pbrt rendering class. [skip ci]

---
+++ python/example/pbrt_renderer_demo.py
@@ -0,0 +1,48 @@
+import sys
+sys.path.append('../')
+
+from pathlib import Path
+import shutil
+import os
+import numpy as np
+
+from py_diff_pd.common.renderer import PbrtRenderer
+from py_diff_pd.common.mesh import voxelize, generate_hex_mesh
+from py_diff_pd.core.py_diff_pd_core import Mesh3d
+from py_diff_pd.common.common import print_info, create_folder
+from py_diff_pd.common.project_path import root_path
+
+if __name__ == '__main__':
+    folder = Path('pbrt_renderer_demo')
+    create_folder(folder)
+
+    # Create a mesh.
+    voxels = np.ones((4, 4, 4))
+    dx = 0.25
+    origin = [0, 0, 0]
+    bin_file_name = '.tmp.bin'
+    generate_hex_mesh(voxels, dx, origin, bin_file_name)
+    mesh = Mesh3d()
+    mesh.Initialize(bin_file_name)
+    os.remove(bin_file_name)
+
+    # Render.
+    options = {
+        'file_name': str(folder / 'demo.png'),
+        'light_map': 'uffizi-large.exr',
+        'sample': 64,
+        'max_depth': 1,
+        'camera_pos': (0, -2, 0.8),
+        'camera_lookat': (0, 0, 0),
+    }
+    renderer = PbrtRenderer(options)
+    renderer.add_hex_mesh(mesh, render_voxel_edge=True, color=(.3, .7, .5), transforms=[
+        ('t', (-1, 0, 0)),
+        ('s', 0.25),
+    ])
+    renderer.add_tri_mesh(Path(root_path) / 'asset/mesh/curved_ground.obj',
+        texture_img='chkbd_24_0.7')
+    renderer.render()
+
+    # Display.
+    os.system('eog {}'.format(folder / 'demo.png'))
