9194bf42225fc22fdc5a26aa9ddefbf89751c20b
Mike Yan Michelis <michelism@ethz.ch>
2021-12-07 16:39:24 +0100

    This direction likely does not work and is too inefficient, keep this as backup

--- python/realbeam_experiments/numerical_damping_compensation_Case_A-1.py
+++ python/realbeam_experiments/numerical_damping_compensation_Case_A-1.py
@@ -9,6 +9,7 @@ from pathlib import Path
 import time
 import os
 import numpy as np
+import scipy
 
 from py_diff_pd.common.common import ndarray, create_folder, print_info,delete_folder
 from py_diff_pd.common.project_path import root_path
@@ -16,6 +17,7 @@ from py_diff_pd.common.renderer import PbrtRenderer
 from py_diff_pd.core.py_diff_pd_core import StdRealVector, HexMesh3d, HexDeformable, TetMesh3d, TetDeformable
 from py_diff_pd.common.hex_mesh import generate_hex_mesh, voxelize, hex2obj
 from py_diff_pd.common.display import render_hex_mesh, export_gif, export_mp4
+
 
 # Utility functions 
 from utils import read_measurement_data, plots_damp_comp_A, create_combined_video
@@ -38,8 +40,10 @@ if __name__ == '__main__':
 
     ### Material and simulation parameters
     # QTM by default captures 100Hz data, dt = h = 0.01
-    dt = [0.25e-2,0.5e-2, 0.75e-2, 1e-2, 1.25e-2, 1.5e-2,1.75e-2, 2e-2]
-    end_frames = [705, 385, 280, 225, 193, 172, 156, 145]
+    # timesteps in decreasing order to make the simulation fast in the beginning (for debugging)
+    timesteps = np.arange(2e-2, 0.24e-2, -0.25e-2)
+    # Number of frames we want to simulate change inversely with timestep
+    frame_nums = (np.ceil(1.6/timesteps)).astype(int)
     
 
     # Material parameters: Dragon Skin 10 
@@ -59,12 +63,11 @@ if __name__ == '__main__':
         'mesh_type': 'hex',
         'refinement': 2.35 
     }
-
+    
     end_frames_idx=0
 
-    for dt in dt:
 
-        frame_num = end_frames[end_frames_idx]-65  # Initial frame not counted
+    for dt, frame_num in zip(timesteps, frame_nums):
         hex_env = BeamEnv(seed, folder, hex_params,0,'A-1', dt)
         hex_deformable = hex_env.deformable()
 
@@ -72,7 +75,7 @@ if __name__ == '__main__':
 
         # Simulation parameters
         methods = ('pd_eigen', )
-        thread_ct = 8
+        thread_ct = 16
         opts = (
             { 'max_pd_iter': 5000, 'max_ls_iter': 10, 'abs_tol': 1e-9, 'rel_tol': 1e-4, 'verbose': 0, 'thread_ct': thread_ct, 'use_bfgs': 1, 'bfgs_history_size': 10 },
         )
@@ -81,17 +84,67 @@ if __name__ == '__main__':
         R, t = hex_env.fit_realframe(qs_real[0])
         qs_real = qs_real @ R.T + t
 
+        
+        def loss_and_grad (lmbda):
+            lmbda = lmbda[0]
+            
+            # Hex Mesh Simulation
+            start = time.time()
+            
+            qs_hex = [] 
+            q_hex, v_hex = hex_env._q0, hex_env._v0
+            
+            qs_hex.append(q_hex)
+            
+            runtime=0
+            for t in range(1, frame_num+1): 
+                # add external force to compensate the numerical damping
+                f_ext=[hex_env.compensate_damping(dt,q=q_hex, v=v_hex, lmbda=lmbda)]
 
+                loss, grad, info = hex_env.simulate(dt, 3, methods[0], opts[0], f_ext=f_ext, require_grad=True, vis_folder=None)
 
-        ### Simulation
+                q_hex = info['q'][1]
+                v_hex = info['v'][1]
+                f_grad = grad[3]
+                
+                import pdb; pdb.set_trace()
+
+                qs_hex.append(q_hex)          
+
+                print(f"Frame {t}/{frame_num} for Hex: {time.time()-start:.2f}s")
+                runtime += time.time()-start
+            
+            
+            print_info(f"Total runtime for hex: {runtime:.2f}")
+            print_info(f"Average runtime per frame: {runtime/frame_num:.2f}")
+        
+
+            print('loss: {:8.4e}, |grad|: {:8.3e}, forward time: {:6.2f}s, backward time: {:6.2f}s, damping parameter: {},'.format(loss, np.linalg.norm(grad), info['forward_time'], info['backward_time'], lmbda))
+
+            return loss, grad
+        
+
+        ### Optimization
         print_info(f"DOFs: {hex_deformable.dofs()} Hex, h={dt}")
+        
+        x_lb = np.ones(1) * 0
+        x_ub = np.ones(1) * 0.5
+        x_init = np.ones(1) * 0.1
+
+        x_bounds = scipy.optimize.Bounds(x_lb, x_ub)
+        
+        t0 = time.time()
+        result = scipy.optimize.minimize(loss_and_grad, np.copy(x_init), method='L-BFGS-B', jac=True, bounds=x_bounds, options={ 'ftol': 1e-8, 'gtol': 1e-8, 'maxiter': 50 })
+        x_fin = result.x[0]
+
+        print(f"Damping Parameter: {x_fin}")
+        import pdb; pdb.set_trace()
 
 
         render_frame_skip = 1
         runtime=0
 
         for method, opt in zip(methods, opts):
-
             qs_hex = [] 
             q_hex, v_hex = hex_env._q0, hex_env._v0
 
@@ -107,11 +160,9 @@ if __name__ == '__main__':
 
             qs_hex.append(q_hex)
 
-            
             print_info("DiffPD Simulation is starting...")
 
             for t in range(1, frame_num+1): 
-
                 # Hex Mesh Simulation
                 start = time.time()
                 # add external force to compensate the numerical damping
